4f74a242fb99a10dd4028ed6a5b67dc37a7a367e
Ivan Tabarino <tabarino@outlook.com>
2023-01-06 22:47:58 +0000

    refactor: move header and footer to separete files

---
+++ src/presentation/components/footer/footer.tsx
@@ -0,0 +1,8 @@
+import React, { memo } from 'react';
+import styles from './footer-styles.scss';
+
+const Footer: React.FC = () => {
+  return <footer className={styles.footer} />;
+};
+
+export default memo(Footer);
