69d3982fb9013663cd74cb506fc46e2c71b146e3
Ivan Tabarino <tabarino@outlook.com>
2023-01-07 18:23:47 +0000

    refactor: change Validation interface

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from '@/presentation/pages';
+import { Validation } from '@/presentation/protocols/validation';
+
+class ValidationSpy implements Validation {
+  errorMessage!: string;
+  fieldName !: string;
+  fieldValue !: string;
+
+  validate(fieldName: string, fieldValue: string): string {
+    this.fieldName = fieldName;
+    this.fieldValue = fieldValue;
+    return this.errorMessage;
+  }
+}
 
 const Router: React.FC = () => {
+
+  const validation = new ValidationSpy();
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login validation={validation} />} />
       </Routes>
     </BrowserRouter>
   );
