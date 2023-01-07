fe5d4c32c4032946fd2cf8270a1b7d49f17730a0
Ivan Tabarino <tabarino@outlook.com>
2023-01-07 10:01:03 +0000

    chore: add jest test environment

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest',
   },
