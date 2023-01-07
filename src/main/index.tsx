2f00e236ec5438ed9701146b6305830655fc3bc5
Ivan Tabarino <tabarino@outlook.com>
2023-01-07 09:53:23 +0000

    fix: move global styles to index

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Router } from '@/presentation/components';
+import '@/presentation/styles/global.scss';
 
 const main = ReactDOM.createRoot(document.getElementById('main') as HTMLElement);
 main.render(
