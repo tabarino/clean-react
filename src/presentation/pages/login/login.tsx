0dd8faa0fc28adc499bcc65e1482149e5d5754bd
Ivan Tabarino <tabarino@outlook.com>
2023-01-08 19:00:00 +0000

    feat: ensure Login add accessToken to localstorage on success

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -61,10 +61,12 @@ const Login: React.FC<Props> = ({ validation, authentication }) => {
         };
       });
 
-      await authentication.auth({
+      const account = await authentication.auth({
         email: state.email,
         password: state.password,
       });
+
+      localStorage.setItem('accessToken', account!.accessToken);
     } catch (error) {
       setState((prevState) => {
         return {
