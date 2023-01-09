f60165384de0562c304eee16a9a90ee6130631cc
Ivan Tabarino <tabarino@outlook.com>
2023-01-09 11:55:13 +0000

    refactor: add FieldValidation on SUT

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -5,31 +5,29 @@ import { ValidationComposite } from './validation-composite';
 
 type SutTypes = {
   sut: ValidationComposite;
+  fieldValidationsSpy: FieldValidation[];
 };
 
-const makeSut = (validators: FieldValidation[]): SutTypes => {
-  const sut = new ValidationComposite(validators);
-  return { sut };
+const makeSut = (field: string): SutTypes => {
+  const fieldValidationsSpy = [new FieldValidationSpy(field), new FieldValidationSpy(field)];
+  const sut = new ValidationComposite(fieldValidationsSpy);
+  return { sut, fieldValidationsSpy };
 };
 
 describe('Validation Composite', () => {
   test('Should return error if any validation fails', () => {
     const field = faker.database.column();
-    const fieldValidationSpy = new FieldValidationSpy(field);
-    const fieldValidationSpy2 = new FieldValidationSpy(field);
-    fieldValidationSpy2.error = new Error('error_message');
-    const { sut } = makeSut([fieldValidationSpy, fieldValidationSpy2]);
+    const { sut, fieldValidationsSpy } = makeSut(field);
+    (fieldValidationsSpy[1] as FieldValidationSpy).error = new Error('error_message');
     const error = sut.validate(field, faker.random.word());
     expect(error).toEqual('error_message');
   });
 
   test('Should return first error message if first validation fails', () => {
     const field = faker.database.column();
-    const fieldValidationSpy = new FieldValidationSpy(field);
-    fieldValidationSpy.error = new Error('first_error_message');
-    const fieldValidationSpy2 = new FieldValidationSpy(field);
-    fieldValidationSpy2.error = new Error('second_error_message');
-    const { sut } = makeSut([fieldValidationSpy, fieldValidationSpy2]);
+    const { sut, fieldValidationsSpy } = makeSut(field);
+    (fieldValidationsSpy[0] as FieldValidationSpy).error = new Error('first_error_message');
+    (fieldValidationsSpy[1] as FieldValidationSpy).error = new Error('second_error_message');
     const error = sut.validate(field, faker.random.word());
     expect(error).toEqual('first_error_message');
   });
