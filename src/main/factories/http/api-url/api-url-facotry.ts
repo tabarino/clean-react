a807d1982a8f346688e945ade6d46a79653f3c68
Ivan Tabarino <tabarino@outlook.com>
2023-01-09 22:38:42 +0000

    refactor: change apu url to receive path by param

--- src/main/factories/http/api-url/api-url-facotry.ts
+++
@@ -1,3 +0,0 @@
-export const makeApiUrl = (): string => {
-  return 'http://localhost:3000/api/login';
-};
--- src/main/factories/usecases/authentication/remote-authentication-factory.ts
+++ src/main/factories/usecases/authentication/remote-authentication-factory.ts
@@ -1,8 +1,8 @@
 import { RemoteAuthentication } from '@/data/usecases';
 import { Authentication } from '@/domain/usecases';
-import { makeApiUrl } from '@/main/factories/http/api-url/api-url-facotry';
+import { makeApiUrl } from '@/main/factories/http/api-url/api-url-factory';
 import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client/axios-http-client-factory';
 
 export const makeRemoteAuthentication = (): Authentication => {
-  return new RemoteAuthentication(makeApiUrl(), makeAxiosHttpClient());
+  return new RemoteAuthentication(makeApiUrl('login'), makeAxiosHttpClient());
 };
