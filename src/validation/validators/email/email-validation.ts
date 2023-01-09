19fb6d8e8058f5c6882cc7944851468445ca35f6
Ivan Tabarino <tabarino@outlook.com>
2023-01-09 10:43:36 +0000

    feat: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -5,6 +5,10 @@ export class EmailValidation implements FieldValidation {
   constructor(public readonly field: string) {}
 
   validate(value: string): Error | null {
-    return new InvalidFieldError();
+    const emailRegex = new RegExp(
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    );
+
+    return emailRegex.test(value) ? null : new InvalidFieldError();
   }
 }
