a557798a088e1bdd7b0eebc0b54f2033e6048a0e
Ivan Tabarino <tabarino@outlook.com>
2023-01-09 12:34:22 +0000

    feat: ensure ValidationBuilder has a email helper

--- src/validation/validators/builder/validation-builder.ts
+++ src/validation/validators/builder/validation-builder.ts
@@ -1,11 +1,16 @@
 import { FieldValidation } from '@/validation/protocols';
-import { RequiredFieldValidation } from '@/validation/validators';
+import { EmailValidation, RequiredFieldValidation } from '@/validation/validators';
 
 export class ValidationBuilder {
   private constructor(private readonly fieldName: string, private readonly validations: FieldValidation[]) {}
 
   static field(fieldName: string): ValidationBuilder {
     return new ValidationBuilder(fieldName, []);
+  }
+
+  email(): ValidationBuilder {
+    this.validations.push(new EmailValidation(this.fieldName));
+    return this;
   }
 
   required(): ValidationBuilder {
