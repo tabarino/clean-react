56866a041436d815b56f6b30a90c0751281f76d6
Ivan Tabarino <tabarino@outlook.com>
2023-01-09 11:20:44 +0000

    test: ensure EmailValidation is valid if email is empty

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -18,6 +18,12 @@ describe('Email Validation', () => {
     expect(error).toEqual(new InvalidFieldError());
   });
 
+  test('Should return falsy if email is empty', () => {
+    const { sut } = makeSut(faker.database.column());
+    const error = sut.validate('');
+    expect(error).toBeFalsy();
+  });
+
   test('Should return falsy if email is valid', () => {
     const { sut } = makeSut(faker.database.column());
     const error = sut.validate(faker.internet.email());
